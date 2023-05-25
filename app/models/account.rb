b20f6df56ee9082f92234104a62ea9398686f62f
Israel <chizunguisrael@gmail.com>
2023-05-25 21:43:50 +0200

    Add bootstrap page structure template

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,6 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
-         
-  has_many: properties
+
+  has_many  :properties
 end
